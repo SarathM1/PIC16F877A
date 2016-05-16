f0c0e9d26aeabb3a1a9bd0f41b52fc3e0bb3b98f
Sarath M <sarathm00@gmail.com>
2016-05-16 22:20:51 +0530

    Header files

---
+++ Code/LCD.h
@@ -0,0 +1,37 @@
+
+// Function Declarations for Generic Functions.c
+
+#ifndef __LCD
+#define __LCD
+
+// Define Pins
+#define LCD_E    			RB0     // Enable pin for LCD
+#define LCD_RS	 			RB1     // RS pin for LCD
+#define LCD_Data_Bus_D4		RB4		// Data bus bit 4
+#define LCD_Data_Bus_D5		RB5		// Data bus bit 5
+#define LCD_Data_Bus_D6		RB6		// Data bus bit 6
+#define LCD_Data_Bus_D7		RB7		// Data bus bit 7
+
+// Define Pins direction registrers
+#define LCD_E_Dir     			TRISB0
+#define LCD_RS_Dir   	 		TRISB1
+#define LCD_Data_Bus_Dir_D4   	TRISB4
+#define LCD_Data_Bus_Dir_D5     TRISB5
+#define LCD_Data_Bus_Dir_D6  	TRISB6
+#define LCD_Data_Bus_Dir_D7 	TRISB7
+
+// Constants
+#define E_Delay       500  
+
+
+// Function Declarations
+void WriteCommandToLCD(unsigned char);
+void WriteDataToLCD(char);
+void InitLCD(void);
+void WriteStringToLCD(const char*);
+void ClearLCDScreen(void);
+void DisplayTimeToLCD(unsigned char*) ;
+void DisplayDateOnLCD( unsigned char*);
+
+
+#endif
