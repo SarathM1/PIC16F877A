cb34f811cbccb8d7df4acd5e9ef47550b871f2ec
Sarath M <sarathm00@gmail.com>
2016-05-16 13:12:18 +0530

    Working

--- Code/ccp_pwm.c
+++ Code/ccp_pwm.c
@@ -12,18 +12,17 @@
 #pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
 #pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
 #pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
-
+#define _XTAL_FREQ 8000000
 void pwm_init()
 {
-    PR2 = 249;
-    CCPR1L = 125 >> 2;
-    CCP1X = 125 & 2;
-    CCP1Y = 125 & 1;
-    T2CKPS1 = 0;
+    PR2 = 249 ;
+    TMR2ON  = 1;
     T2CKPS0 = 1;
+    CCPR1L = 124;
+    CCP1X = 1;
+    CCP1Y = 1;
     CCP1M3 = 1;
     CCP1M2 = 1;
-    TMR2ON = 1;
 }
 
 void main()
