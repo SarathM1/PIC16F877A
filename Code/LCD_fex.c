ba8085d92f46b6a2b9c6ea2b20908ee07916de29
Sarath M <sarathm00@gmail.com>
2017-08-13 15:07:48 +0530

    LCD test code working

---
+++ Code/LCD_fex.c
@@ -0,0 +1,107 @@
+#include "LCD_fex.h"
+#include <xc.h>
+#define _XTAL_FREQ 2000000
+
+void ToggleEpinOfLCD(void)
+{
+	LCD_E = 1;                // Give a pulse on E pin
+	__delay_us(E_Delay);      // so that LCD can latch the
+	LCD_E = 0;                // data from data bus
+	__delay_us(E_Delay); 	
+}
+
+
+void WriteCommandToLCD(unsigned char Command)  
+{
+	LCD_RS = 0;				  // It is a command
+	
+	PORTB &= 0x0F;			  // Make Data pins zero
+	PORTB |= (Command&0xF0);  // Write Upper nibble of data
+	ToggleEpinOfLCD();		  // Give pulse on E pin	
+	
+	PORTB &= 0x0F;			  // Make Data pins zero
+	PORTB |= ((Command<<4)&0xF0); // Write Lower nibble of data
+	ToggleEpinOfLCD();		  // Give pulse on E pin
+}
+
+
+void WriteDataToLCD(char LCDChar)  
+{
+	LCD_RS = 1;				  // It is data
+	
+	PORTB &= 0x0F;			  // Make Data pins zero
+	PORTB |= (LCDChar&0xF0);  // Write Upper nibble of data
+	ToggleEpinOfLCD();		  // Give pulse on E pin	
+	
+	PORTB &= 0x0F;			  // Make Data pins zero
+	PORTB |= ((LCDChar<<4)&0xF0); // Write Lower nibble of data
+	ToggleEpinOfLCD();		  // Give pulse on E pin
+}
+
+
+void InitLCD(void)
+{ 
+	// Firstly make all pins output
+	LCD_E  		 		 = 0;   // E  = 0
+	LCD_RS  	 		 = 0;   // RS = 0
+	LCD_Data_Bus_D4		 = 0;  	// Data bus = 0
+	LCD_Data_Bus_D5		 = 0;  	// Data bus = 0
+	LCD_Data_Bus_D6		 = 0;  	// Data bus = 0
+	LCD_Data_Bus_D7		 = 0;  	// Data bus = 0
+	LCD_E_Dir    		 = 0;   // Make Output
+	LCD_RS_Dir    	 	 = 0;   // Make Output
+	LCD_Data_Bus_Dir_D4  = 0;   // Make Output
+	LCD_Data_Bus_Dir_D5  = 0;   // Make Output
+	LCD_Data_Bus_Dir_D6  = 0;   // Make Output
+	LCD_Data_Bus_Dir_D7  = 0;   // Make Output
+
+  ///////////////// Reset process from datasheet //////////////
+   __delay_ms(40);
+   
+	PORTB &= 0x0F;			  // Make Data pins zero
+	PORTB |= 0x30;			  // Write 0x3 value on data bus
+	ToggleEpinOfLCD();		  // Give pulse on E pin
+
+   __delay_ms(6);
+   
+	PORTB &= 0x0F;			  // Make Data pins zero
+	PORTB |= 0x30;			  // Write 0x3 value on data bus
+	ToggleEpinOfLCD();		  // Give pulse on E pin
+
+   __delay_us(300);
+   
+	PORTB &= 0x0F;			  // Make Data pins zero
+	PORTB |= 0x30;			  // Write 0x3 value on data bus
+	ToggleEpinOfLCD();		  // Give pulse on E pin
+
+   __delay_ms(2);
+   
+	PORTB &= 0x0F;			  // Make Data pins zero
+	PORTB |= 0x20;			  // Write 0x2 value on data bus
+	ToggleEpinOfLCD();		  // Give pulse on E pin
+	
+	__delay_ms(2);
+  /////////////// Reset Process End ////////////////
+	WriteCommandToLCD(0x28);    //function set
+	WriteCommandToLCD(0x0c);    //display on,cursor off,blink off
+	WriteCommandToLCD(0x01);    //clear display
+	WriteCommandToLCD(0x06);    //entry mode, set increment
+}
+
+
+void WriteStringToLCD(const char *s)
+{
+	while(*s)
+		WriteDataToLCD(*s++);   // print first character on LCD 
+}
+
+
+void ClearLCDScreen(void)       // Clear the Screen and return cursor to zero position
+{
+	WriteCommandToLCD(0x01);    // Clear the screen
+	__delay_ms(2);              // Delay for cursor to return at zero position
+}
+
+
+
+
---
+++ main.c
@@ -0,0 +1,38 @@
+/*
+ * File:   main.c
+ * Author: windows 7
+ *
+ * Created on August 13, 2017, 2:08 PM
+ */
+
+#include <xc.h>
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+// CONFIG
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bit (BOR disabled)
+#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+#define _XTAL_FREQ 2000000
+
+
+#include "Code/LCD_fex.h"
+
+
+
+void main(void) {
+    TRISC = 0x00;
+    PORTC = 0x00;
+    InitLCD();
+    ClearLCDScreen();
+    WriteStringToLCD("Hi World");
+    RC0 = 1;
+    while(1);
+    return;
+}
